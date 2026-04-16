ee96561a2352b42686664e6644989a4584b35a71
YunjeJang <jjw1270@naver.com>
2026-04-16 19:25:59 +0900

    omx(team): auto-checkpoint worker-2 [2]

--- Plugins/InteractionSystem/Source/InteractionSystem/Private/Tests/InteractorComponentTests.cpp
+++ Plugins/InteractionSystem/Source/InteractionSystem/Private/Tests/InteractorComponentTests.cpp
@@ -6,26 +6,36 @@
 IMPLEMENT_SIMPLE_AUTOMATION_TEST(
 	FInteractorComponentDetectableRangeClampTest,
 	"InteractionSystem.InteractorComponent.DetectableRangeClampsToTargetableRange",
-	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::EngineFilter)
+	EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
 
 bool FInteractorComponentDetectableRangeClampTest::RunTest(const FString& Parameters)
 {
-	UInteractorComponent* interactor_component = NewObject<UInteractorComponent>();
-	TestNotNull(TEXT("InteractorComponent should be created"), interactor_component);
-	if (interactor_component == nullptr)
+	UInteractorComponent* clamped_interactor_component = NewObject<UInteractorComponent>();
+	TestNotNull(TEXT("Clamp test interactor component should be created"), clamped_interactor_component);
+	if (clamped_interactor_component == nullptr)
 	{
 		return false;
 	}
 
-	TestEqual(TEXT("Default sphere radius should match the default detectable range"), interactor_component->GetUnscaledSphereRadius(), 600.0f);
+	TestEqual(TEXT("Default sphere radius should match the default detectable range"), clamped_interactor_component->GetUnscaledSphereRadius(), 600.0f);
 
-	interactor_component->SetTargetableRange(700.0f);
-	interactor_component->SetDetectableRange(500.0f);
+	AddExpectedError(TEXT("상호작용 가능 거리가 감지거리보다 클 수 없습니다!"), EAutomationExpectedErrorFlags::Contains, 2);
+	clamped_interactor_component->SetTargetableRange(700.0f);
 
-	TestEqual(TEXT("Detectable range should clamp up to the already-clamped targetable range"), interactor_component->GetUnscaledSphereRadius(), 600.0f);
+	AddExpectedError(TEXT("감지거리가 상호작용 가능 거리보다 작을 수 없습니다!"), EAutomationExpectedErrorFlags::Contains, 2);
+	clamped_interactor_component->SetDetectableRange(500.0f);
 
-	interactor_component->SetDetectableRange(450.0f);
-	TestEqual(TEXT("Valid detectable range should update the sphere radius"), interactor_component->GetUnscaledSphereRadius(), 450.0f);
+	TestEqual(TEXT("Detectable range should clamp up to the already-clamped targetable range"), clamped_interactor_component->GetUnscaledSphereRadius(), 600.0f);
+
+	UInteractorComponent* valid_interactor_component = NewObject<UInteractorComponent>();
+	TestNotNull(TEXT("Valid range test interactor component should be created"), valid_interactor_component);
+	if (valid_interactor_component == nullptr)
+	{
+		return false;
+	}
+
+	valid_interactor_component->SetDetectableRange(450.0f);
+	TestEqual(TEXT("Valid detectable range should update the sphere radius"), valid_interactor_component->GetUnscaledSphereRadius(), 450.0f);
 
 	return true;
 }
@@ -33,7 +43,7 @@ bool FInteractorComponentDetectableRangeClampTest::RunTest(const FString& Parame
 IMPLEMENT_SIMPLE_AUTOMATION_TEST(
 	FInteractorComponentDetectModeSetterTest,
 	"InteractionSystem.InteractorComponent.DetectModeSetterUpdatesState",
-	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::EngineFilter)
+	EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
 
 bool FInteractorComponentDetectModeSetterTest::RunTest(const FString& Parameters)
 {
