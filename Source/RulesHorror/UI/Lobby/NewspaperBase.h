759462147a8e5e41e5e4dd8fa5893b6de6e73c28
YunjeJang <jjw1270@naver.com>
2026-04-22 20:00:22 +0900

    로비 작업중..

---
+++ Source/RulesHorror/UI/Lobby/NewspaperBase.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2026 장윤제. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/WidgetBase.h"
+#include "NewspaperBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS(abstract)
+class RULESHORROR_API UNewspaperBase : public UWidgetBase
+{
+	GENERATED_BODY()
+	
+
+};
